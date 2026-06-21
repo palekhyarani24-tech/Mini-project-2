80df20c7a32e2b6e4ecc9de320b42db44ebb4eb5
palekhyarani24-tech <palekhyarani24@gmail.com>
2026-06-21 12:37:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+use("alumniDB");
+
+db.alumni.insertOne({
+  name: "Rahul Sharma",
+  graduationYear: 2022,
+  course: "BCA",
+  email: "rahul@example.com",
+  company: "Infosys",
+  designation: "Software Engineer"
+});
